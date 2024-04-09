b174b07520e211fe5a4f4e188c6b0b7f490f0c49
Karym <karym.yousfi@gmail.com>
2024-04-09 10:13:43 +0200

    fix SOKOBAN

---
+++ Sokoban/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    public int speed = 5;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+
+    }
+
+}
