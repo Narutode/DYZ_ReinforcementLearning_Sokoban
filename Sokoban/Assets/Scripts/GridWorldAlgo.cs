a6ba3d92505c48a4d51a447cd73c4e5e855f1efe
Soorry <77273729+Soorry@users.noreply.github.com>
2024-04-09 14:50:17 +0200

    Ajout interface

--- Sokoban/Assets/Scripts/GridWorldAlgo.cs
+++ Sokoban/Assets/Scripts/GridWorldAlgo.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class GridWorldAlgo : MonoBehaviour
+public class GridWorldAlgo : MonoBehaviour, I_DPL
 {
     public GameObject floor;
     public GameObject wall;
@@ -16,32 +17,33 @@ public class GridWorldAlgo : MonoBehaviour
 
     float gamma = 0.5f;
     float deltaLimit = 0.0001f;
-    bool policy = true;
+    bool policy = false;
 
     public int sizeX = 5, sizeY = 5;
 
-    private int[,] grid = {  { 0, 0, 0, 0, 0 , 0, 0,0 },
+    private int[,] grid = { { 0, 0, 0, 0, 0, 0, 0, 0},
                             { 0, 1, 1, 1, 0, 0, 1, 0},
                             { 0, 0, 0, 0, 0, 0, 0, 1},
-                            { 0, 1, 0, 1, 0, 0, 1, 1 },
-                            { 1, 1, 0, 1, 0 , 0, 1, 0},
-                            { 2, 0, 0, 0, 0 , 0, 0, 3}};
+                            { 0, 1, 0, 1, 0, 0, 1, 1},
+                            { 1, 1, 0, 1, 0, 0, 1, 0},
+                            { 2, 0, 0, 0, 1, 0, 0, 3} };
 
-    private float[,] gridValue;
+    public List<state> states = new List<state>();
 
-    private int[,] gridPolicy;
+    public MDP mdp;
 
     // Start is called before the first frame update
     void Start()
     {
-        gridValue = new float[sizeX, sizeY];
-        gridPolicy = new int[sizeX, sizeY];
         for (int x = 0; x < sizeX; x++)
         {
             for (int y = 0; y < sizeY; y++)
             {
-                gridValue[x, y] = 0;
-                gridPolicy[x, y] = Random.Range(0, 4);
+                state newState = new state();
+                newState.key = new List<int> { x, y };
+                newState.value = 0;
+                newState.policy = Random.Range(0, 4);
+                states.Add(newState);
                 GameObject inst = null;
                 switch (grid[x, y])
                 {
@@ -68,11 +70,12 @@ public class GridWorldAlgo : MonoBehaviour
                 }
             }
         }
+        mdp = new MDP(this);
         //Value Iteration
         if (!policy)
         {
-            allValueEvaluation();
-            PolicyImprovement();
+            mdp.allValueEvaluation();
+            mdp.PolicyImprovement();
             drawArrows();
         }
     }
@@ -84,193 +87,39 @@ public class GridWorldAlgo : MonoBehaviour
         if (policy)
         {
             deleteArrows();
-            PolicyEvaluation();
+            mdp.PolicyEvaluation();
             drawArrows();
-            policy = !PolicyImprovement();
+            policy = !mdp.PolicyImprovement();
         }
-    }
-
-    void PolicyEvaluation()
-    {
-        float delta = 0;
-        while (true)
-        {
-            delta = 0;
-            //float[,] gridValueTemp = new float[sizeX, sizeY];
-            for (int x = 0; x < sizeX; x++)
-            {
-                for (int y = 0; y < sizeY; y++)
-                {
-                    if (grid[x, y] == 1) //Cas wall
-                    {
-                        gridValue[x, y] = 0;
-                        continue;
-                    }
-                    float vPrime = 0;
-                    float prevValue = gridValue[x, y];
-                    switch (gridPolicy[x, y])
-                    {
-                        case 0:
-                            if (y - 1 >= 0)
-                                if (grid[x, y - 1] != 1)
-                                    vPrime = gridValue[x, y - 1];
-                            break;
-                        case 1:
-                            if (x - 1 >= 0)
-                                if (grid[x - 1, y] != 1)
-                                    vPrime = gridValue[x - 1, y];
-                            break;
-                        case 2:
-                            if (y + 1 < sizeY)
-                                if (grid[x, y + 1] != 1)
-                                    vPrime = gridValue[x, y + 1];
-                            break;
-                        case 3:
-                            if (x + 1 < sizeX)
-                                if (grid[x + 1, y] != 1)
-                                    vPrime = gridValue[x + 1, y];
-                            break;
-                    }            
-                    gridValue[x, y] = reward(x, y) + gamma * vPrime;
-                    delta = Mathf.Max(delta, Mathf.Abs(prevValue - gridValue[x, y]));
-                }
-            }
-            if (delta < deltaLimit)
-                break;
-        }
-    }
-
-    void allValueEvaluation()
-    {
-        float delta = 0;
-        while (true)
-        {
-            delta = 0;
-            for (int x = 0; x < sizeX; x++)
-            {
-                for (int y = 0; y < sizeY; y++)
-                {
-                    if (grid[x, y] == 1)
-                    {
-                        gridValue[x, y] = 0;
-                        continue;
-                    }
-                    float vPrimeMax = 0;
-                    float prevValue = gridValue[x, y];
-
-                    if (y - 1 >= 0)
-                        if (grid[x, y - 1] != 1)
-                            if(gridValue[x, y - 1] > vPrimeMax)
-                                vPrimeMax = gridValue[x, y - 1];
-                    if (x - 1 >= 0)
-                        if (grid[x - 1, y] != 1)
-                            if (gridValue[x - 1, y] > vPrimeMax)
-                                vPrimeMax = gridValue[x - 1, y];
-                    if (y + 1 < sizeY)
-                        if (grid[x, y + 1] != 1)
-                            if (gridValue[x, y + 1] > vPrimeMax)
-                                vPrimeMax = gridValue[x, y + 1];
-                    if (x + 1 < sizeX)
-                        if (grid[x + 1, y] != 1)
-                            if (gridValue[x + 1, y] > vPrimeMax)
-                                vPrimeMax = gridValue[x + 1, y];
-
-                    gridValue[x, y] = reward(x, y) + gamma * vPrimeMax;
-                    delta = Mathf.Max(delta, Mathf.Abs(prevValue - gridValue[x, y]));
-                }
-            }
-            if (delta < deltaLimit)
-                break;
-        }
-    }
-
-    bool PolicyImprovement()
-    {
-        bool stable = true;
-        for (int x = 0; x < sizeX; x++)
-        {
-            for (int y = 0; y < sizeY; y++)
-            {
-                int temp = gridPolicy[x, y];
-                float valMax = 0;
-                int newP = temp;
-                if (y - 1 >= 0)
-                {
-                    if(gridValue[x, y - 1] > valMax)
-                    {
-                        valMax = gridValue[x, y - 1];
-                        newP = 0;
-                    }
-                }
-                if (x - 1 >= 0)
-                {
-                    if (gridValue[x - 1, y] > valMax)
-                    {
-                        valMax = gridValue[x - 1, y];
-                        newP = 1;
-                    }
-                }
-                if (y + 1 < sizeY)
-                {
-                    if (gridValue[x, y + 1] > valMax)
-                    {
-                        valMax = gridValue[x, y + 1];
-                        newP = 2;
-                    }
-                }
-                if (x + 1 < sizeX)
-                {
-                    if (gridValue[x + 1, y] > valMax)
-                    {
-                        valMax = gridValue[x + 1, y];
-                        newP = 3;
-                    }
-                }
-                if (temp != newP)
-                {
-                    stable = false;
-                    gridPolicy[x, y] = newP;
-                }
-            }
-        }
-        return stable;
-    }
-
-    float reward(int x, int y)
-    {
-        if (grid[x, y] == 3)
-            return 1;
-        else return 0;
     }
 
     void drawArrows()
     {
-        for (int x = 0; x < sizeX; x++)
-        {
-            for (int y = 0; y < sizeY; y++)
-            {
-                if (grid[x, y] != 0)
+        foreach(var st in states) {
+            int x = st.key[0];
+            int y = st.key[1];
+
+            if (grid[x, y] != 0)
                     continue;
-                GameObject inst;
-                switch (gridPolicy[x, y])
-                {
-                    case 0:
-                        inst = Instantiate(arrow0, arrowParent.transform);
-                        inst.transform.position = new Vector3(x-sizeX/2, y-sizeY/2, -1);
-                        break;
-                    case 1:
-                        inst = Instantiate(arrow1, arrowParent.transform);
-                        inst.transform.position = new Vector3(x-sizeX/2, y-sizeY/2, -1);
-                        break;
-                    case 2:
-                        inst = Instantiate(arrow2, arrowParent.transform);
-                        inst.transform.position = new Vector3(x-sizeX/2, y-sizeY/2, -1);
-                        break;
-                    case 3:
-                        inst = Instantiate(arrow3, arrowParent.transform);
-                        inst.transform.position = new Vector3(x-sizeX/2, y-sizeY/2, -1);
-                        break;
-                }
+            GameObject inst;
+            switch (st.policy)
+            {
+                case 0:
+                    inst = Instantiate(arrow0, arrowParent.transform);
+                    inst.transform.position = new Vector3(x-sizeX/2, y-sizeY/2, -1);
+                    break;
+                case 1:
+                    inst = Instantiate(arrow1, arrowParent.transform);
+                    inst.transform.position = new Vector3(x-sizeX/2, y-sizeY/2, -1);
+                    break;
+                case 2:
+                    inst = Instantiate(arrow2, arrowParent.transform);
+                    inst.transform.position = new Vector3(x-sizeX/2, y-sizeY/2, -1);
+                    break;
+                case 3:
+                    inst = Instantiate(arrow3, arrowParent.transform);
+                    inst.transform.position = new Vector3(x-sizeX/2, y-sizeY/2, -1);
+                    break;
             }
         }
     }
@@ -281,5 +130,52 @@ public class GridWorldAlgo : MonoBehaviour
         {
             Destroy(arrowParent.transform.GetChild(i).gameObject);
         }
+    }
+
+    public List<state> getStates()
+    {
+        return states;
+    }
+
+    public List<int> getActions()
+    {
+        return new List<int> { 0, 1, 2, 3 };
+    }
+
+    public state getNextState(state st, int action)
+    {
+        int x = st.key[0];
+        int y = st.key[1];
+        switch (action)
+        {
+            case 0:
+                if (y - 1 >= 0)
+                    if (grid[x, y - 1] != 1)
+                        return states.First(t => t.key[0] == x && t.key[1] == y-1);
+                break;
+            case 1:
+                if (x - 1 >= 0)
+                    if (grid[x - 1, y] != 1)
+                        return states.First(t => t.key[0] == x-1 && t.key[1] == y);
+                break;
+            case 2:
+                if (y + 1 < sizeY)
+                    if (grid[x, y + 1] != 1)
+                        return states.First(t => t.key[0] == x && t.key[1] == y+1);
+                break;
+            case 3:
+                if (x + 1 < sizeX)
+                    if (grid[x + 1, y] != 1)
+                        return states.First(t => t.key[0] == x+1 && t.key[1] == y);
+                break;
+        }
+        return null;
+    }
+
+    public float getReward(state st)
+    {
+        if (grid[st.key[0], st.key[1]] == 3)
+            return 1;
+        else return 0;
     }
 }
