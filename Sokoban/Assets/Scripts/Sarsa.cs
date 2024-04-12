8ef95ca06629b353dea8c815fa00c2eaa6e5da2a
Soorry <77273729+Soorry@users.noreply.github.com>
2024-04-12 16:16:03 +0200

    add sarsa

--- Sokoban/Assets/Scripts/Sarsa.cs
+++ Sokoban/Assets/Scripts/Sarsa.cs
@@ -1,40 +1,54 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-/*
+
 public class Sarsa
 {
+    Dictionary<Tuple<state, int>, float> Qvalue = new Dictionary<Tuple<state, int>, float>();
+    state firstState;
     float alpha = 0.1f;
     float gamma = 0.9f;
     float epsilon = 0.1f;
-    public void Train(int episodes)
+    I_DPL game;
+
+    public Sarsa(I_DPL g)
     {
-        for (int episode = 0; episode < episodes; episode++)
+        game = g;
+        firstState = game.getFirstState();
+        List<state> states = game.getStates();
+
+        foreach(var st in states)
         {
-            // Début d'un épisode
-            int state = environment.Reset(); // État initial
-            int action = EpsilonGreedy(state, environment.GetNumActions());
-
-            while (!environment.IsTerminal(state))
+            for(int i = 0; i < 4; i++)
             {
-                // Étape de l'épisode
-                Tuple<int, float> nextStateReward = environment.Step(action);
-                int nextState = nextStateReward.Item1;
-                float reward = nextStateReward.Item2;
-
-                int nextAction = EpsilonGreedy(nextState, environment.GetNumActions());
-
-                // Mise à jour de la valeur Q
-                QValues[state, action] += alpha * (reward + gamma * QValues[nextState, nextAction] - QValues[state, action]);
-
-                state = nextState;
-                action = nextAction;
+                Qvalue.Add(new Tuple<state, int>(st, i), 0);
             }
         }
     }
 
-    private int EpsilonGreedy(int state, int numActions)
+    public void Train(int episodes)
+    {
+        for (int episode = 0; episode < episodes; episode++)
+        {
+            state state = firstState;
+            int action = EpsilonGreedy(state, 4);
+            float reward = game.getReward(state);
+            while (reward < 1)
+            {
+                state nextState = game.getNextState(state, action);
+                reward = game.getReward(nextState);
+                var valState = Qvalue[new Tuple<state, int>(state, action)];
+                var nextAction = EpsilonGreedy(state, 4);
+                var valNextState = Qvalue[new Tuple<state, int>(nextState, nextAction)];
+                valState += alpha * (reward + gamma * valNextState - valState);
+                Qvalue[new Tuple<state, int>(state, action)] = valState;
+            }
+        }
+    }
+
+    private int EpsilonGreedy(state state, int numActions)
     {
         // Politique epsilon-greedy
         if (UnityEngine.Random.Range(0f, 1f) < epsilon)
@@ -47,10 +61,9 @@ public class Sarsa
             float[] qValuesForState = new float[numActions];
             for (int a = 0; a < numActions; a++)
             {
-                qValuesForState[a] = QValues[state, a];
+                qValuesForState[a] = Qvalue.GetValueOrDefault(new Tuple<state, int>(state, a));
             }
             return Array.IndexOf(qValuesForState, qValuesForState.Max());
         }
     }
 }
-*/
